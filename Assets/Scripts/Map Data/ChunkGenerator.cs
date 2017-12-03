148d140e50b296c146c4a253c42029309fe2c2a6
Daniel Everland <daniel@everlandgames.com>
2017-12-03 17:49:49 +0100

    Removed slow algorithms

--- Assets/Scripts/Map Data/ChunkGenerator.cs
+++ Assets/Scripts/Map Data/ChunkGenerator.cs
@@ -13,79 +13,8 @@ public class ChunkGenerator {
     private static List<Vector3> vertices = new List<Vector3>();
     private static List<List<int>> triangles = new List<List<int>>();
     private static List<Material> materials = new List<Material>();
-    private static Texture2D chunkTexture;
-    private static TileType currentTile;
-    private static Material currentMaterial;
-
-    static System.Diagnostics.Stopwatch stopWatch;
 
 	public static void RenderChunk(Chunk chunk)
-    {
-        stopWatch = new System.Diagnostics.Stopwatch();
-
-
-        RunTest(RenderChunkUsingSpriteRenderer, "Sprite");
-        RunTest(RenderChunkUsingFastMeshRendering, "Fast Mesh");
-        RunTest(RenderChunkUsingMeshRenderer, "Mesh");
-    }
-    private static void RunTest(Action<Chunk> callback, string type)
-    {
-        List<long> milliseconds = new List<long>();
-
-        for (int i = 0; i < 100; i++)
-        {
-            Chunk newChunk = new Chunk();
-
-            stopWatch.Reset();
-            stopWatch.Start();
-
-            callback(new Chunk());
-
-            stopWatch.Stop();
-            milliseconds.Add(stopWatch.ElapsedMilliseconds);
-        }
-
-        Debug.Log(type + " took " + Mathf.RoundToInt((float)milliseconds.Average()) + " milliseconds");
-    }
-    private static void RenderChunkUsingFastMeshRendering(Chunk chunk)
-    {
-        currentMeshFilter = ChunkObjectPool.GetMeshFilter();
-        currentMesh = new Mesh();
-        chunkTexture = new Texture2D(Chunk.PIXELS_PER_TILE * Chunk.CHUNK_SIZE, Chunk.PIXELS_PER_TILE * Chunk.CHUNK_SIZE);
-
-        for (int x = 0; x < Chunk.CHUNK_SIZE; x++)
-        {
-            for (int y = 0; y < Chunk.CHUNK_SIZE; y++)
-            {
-                currentTile = TileType.AllTypes[chunk.Tiles[x, y]];
-
-                chunkTexture.SetPixels(x * Chunk.PIXELS_PER_TILE, y * Chunk.PIXELS_PER_TILE, Chunk.PIXELS_PER_TILE, Chunk.PIXELS_PER_TILE, currentTile.Texture.GetPixels());
-            }
-        }
-
-        chunkTexture.Apply();
-        currentMaterial = new Material(Shader.Find("Standard"));
-        currentMaterial.mainTexture = chunkTexture;
-
-        currentMesh.vertices = new Vector3[4]
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(Chunk.CHUNK_SIZE, 0, 0),
-            new Vector3(Chunk.CHUNK_SIZE, Chunk.CHUNK_SIZE, 0),
-            new Vector3(0, Chunk.CHUNK_SIZE, 0),
-        };
-
-        currentMesh.triangles = new int[6]
-        {
-            3, 0, 1,
-            2, 0, 3,
-        };
-
-        currentMesh.RecalculateNormals();
-        currentMeshFilter.mesh = currentMesh;
-        currentMeshFilter.GetComponent<MeshRenderer>().material = currentMaterial;
-    }
-    private static void RenderChunkUsingMeshRenderer(Chunk chunk)
     {
         currentMeshFilter = ChunkObjectPool.GetMeshFilter();
         currentMesh = new Mesh();
@@ -109,7 +38,7 @@ public class ChunkGenerator {
         {
             currentMesh.SetTriangles(triangles[i], i);
         }
-        
+
         currentMesh.RecalculateNormals();
 
         currentMeshFilter.mesh = currentMesh;
@@ -143,23 +72,5 @@ public class ChunkGenerator {
         });
 
         verticeCount += 4;
-    }
-    private static void RenderChunkUsingSpriteRenderer(Chunk chunk)
-    {
-        chunkTexture = new Texture2D(Chunk.PIXELS_PER_TILE * Chunk.CHUNK_SIZE, Chunk.PIXELS_PER_TILE * Chunk.CHUNK_SIZE);
-
-        for (int x = 0; x < Chunk.CHUNK_SIZE; x++)
-        {
-            for (int y = 0; y < Chunk.CHUNK_SIZE; y++)
-            {
-                currentTile = TileType.AllTypes[chunk.Tiles[x, y]];
-
-                chunkTexture.SetPixels(x * Chunk.PIXELS_PER_TILE, y * Chunk.PIXELS_PER_TILE, Chunk.PIXELS_PER_TILE, Chunk.PIXELS_PER_TILE, currentTile.Texture.GetPixels());
-            }
-        }
-
-        chunkTexture.Apply();
-
-        ChunkObjectPool.GetSpriteRenderer().sprite = Sprite.Create(chunkTexture, new Rect(0, 0, chunkTexture.width, chunkTexture.height), Vector2.one / 2);
     }
 }
