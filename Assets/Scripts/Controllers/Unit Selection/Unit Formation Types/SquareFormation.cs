02e08acddb2b531069919a2fdf680ace50aae594
Daniel Everland <daniel@everlandgames.com>
2017-12-20 05:56:29 +0100

    Fixed square formation not being rectangular

--- Assets/Scripts/Controllers/Unit Selection/Unit Formation Types/SquareFormation.cs
+++ Assets/Scripts/Controllers/Unit Selection/Unit Formation Types/SquareFormation.cs
@@ -4,33 +4,24 @@ using UnityEngine;
 
 public class SquareFormation : IUnitFormation
 {
-    private PriorityQueue<Vector2, float> priorityQueue = new PriorityQueue<Vector2, float>();
+    private float SQUARE_ELONGATION = 2;
 
     public IEnumerable<Vector2> GetPattern(int unitCount)
     {
-        priorityQueue.Clear();
+        List<Vector2> toReturn = new List<Vector2>(unitCount);
 
-        int radius = Mathf.CeilToInt(Mathf.Sqrt(unitCount) / 2);
+        float radius = Mathf.Sqrt(unitCount) / 2;
+        int xSize = Mathf.CeilToInt(radius * SQUARE_ELONGATION);
+        int ySize = Mathf.CeilToInt(radius / SQUARE_ELONGATION);
 
-        for (int x = -radius; x < radius; x++)
+        for (int x = -xSize; x <= xSize; x++)
         {
-            for (int y = -radius; y < radius; y++)
+            for (int y = -ySize; y <= ySize; y++)
             {
-                priorityQueue.Enqueue(new Vector2(x, y), GetPriority(x, y));
+                toReturn.Add(new Vector2(x, y));
             }
         }
 
-        List<Vector2> pattern = new List<Vector2>();
-
-        for (int i = 0; i < unitCount; i++)
-        {
-            pattern.Add(priorityQueue.Dequeue());
-        }
-
-        return pattern;
-    }
-    private float GetPriority(int x, int y)
-    {
-        return Mathf.Abs(x) + Mathf.Abs(y * y);
+        return toReturn;
     }
 }
